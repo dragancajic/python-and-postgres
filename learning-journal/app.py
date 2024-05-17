62007769a00e49b39311df812bad4c0236e2e9b1
Драган Ћајић <cajic_dragan@yahoo.com>
2024-05-17 16:25:58 +0200

    ~ `Programming Diary`: connect to SQLite with Python
    § The Complete Python🐍/PostgreSQL🐘 Course 2.0
    https://pysql.tecladocode.com/
    
    › use `sqlite3` module;
    › create one connection;
    › use `context manager`.
    
    ■ What is a transaction?
    
    A **transaction** is a sequence of operations performed (using one or more SQL statements)
    on a database as a `single logical unit of work`.
    
    The effects of all the SQL statements in a **transaction** can be either ALL *committed*
    (applied to the database) OR all *rolled back* (undone from the database)!
    
    The easiest way to handle both *committing* and *rolling back* in one go is by using
    the **context manager**:
    with connection:  # committing and/or rolling back using `context manager`!
        connection.execute("CREATE TABLE entry (content TEXT, date TEXT);")
    
    When you use the **context manager***, `with connection`, it'll automatically *commit*
    for you at the end of the *context manager*, or *roll back* if an error was encountered!
    
    » Learning Journal/Programming Diary with Python & SQLite :snake: :minidisc:
    › 03-07 How to connect to SQLite with Python | The Complete Python/PostgreSQL Course 2.0
    https://pysql.tecladocode.com/section03/lectures/07_connect_sqlite_python/

--- learning-journal/app.py
+++ learning-journal/app.py
@@ -3,15 +3,8 @@
 
 Python file that contains all the application logic.
 '''
-
-# 03-04 Using Python lists as a database | The Complete Python/PostgreSQL Course 2.0
-# https://pysql.tecladocode.com/section03/lectures/04_python_lists_as_database/
-
-# ~ Using the database in app.py
-
-# Make use of the data store.
-# We need to use the data store instead of print "Adding..." or "Viewing...".
 from database import add_entry, get_entries
+from database import close_connection
 
 WELCOME = "Welcome to the Learning Journal!"
 print('\n' + WELCOME)
@@ -24,10 +17,6 @@ MENU = """\nPlease select one of the following options:
 
 Your selection: """
 
-
-# ~ Modify `app.py` so that it does the user interaction only
-
-# We could improve the code inside `app.py` further by splitting it into functions:
 def prompt_new_entry():
     '''Function for interacting with the user in order to add new entry.'''
     entry_content = input("\nWhat have you learned today? ")
@@ -51,5 +40,10 @@ while (user_input := input(MENU)) != "3":
         view_entries(get_entries())
     else:
         print("Invalid option, please try again!")
+
+
+# Now, in `app.py` we must call `close_connection()` when the user exits the application.
+# This is only called when the `while` loop ends.
+close_connection()
 
 print("\nGoodbye! See you next time! ;-)\n")
--- learning-journal/database.py
+++ learning-journal/database.py
@@ -4,6 +4,40 @@
 Python file that contains the logic that interacts with the data store.
 For now the data store is a list, but later on it'll be a database.
 '''
+import sqlite3  # use this built-in module to interact with SQLite from within Python
+
+# connection object here is more useful, then inside the function
+connection = sqlite3.connect('data.db')
+
+def create_table():
+    """
+    Function that will be in charge of creating the table.
+
+    ~ What is a transaction?
+
+    A **transaction** is a sequence of operations performed
+    (using one or more SQL statements) on a database as a `single logical unit of work`.
+
+    The effects of all the SQL statements in a **transaction** can be either ALL
+    *committed* (applied to the database) OR all *rolled back* (undone from the database).
+    """
+    # The easiest way to handle both *committing* and *rolling back*
+    # in one go is by using the **context manager**:
+    with connection:  # committing and/or rolling back using `context manager`!
+        connection.execute("CREATE TABLE entry (content TEXT, date TEXT);")
+    # When you use the **context manager***, `with connection`, it'll automatically
+    # *commit* for you at the end of the *context manager*, or *roll back* if an error
+    # was encountered.
+
+
+def close_connection():
+    """
+    ALWAYS close connection, when you've finished working with DB!
+    """
+    connection.close()
+    print("Connection to the DB is closed!")
+
+
 entries = []
 
 # Decoupling data from display
