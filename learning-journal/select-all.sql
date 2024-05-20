334abcfed1fcf20a4b42d0bc80e25570ebed755e
Драган Ћајић <cajic_dragan@yahoo.com>
2024-05-20 23:58:11 +0200

    ~ `Programming Diary`: retrieve data from DB with SQL
    § The Complete Python🐍/PostgreSQL🐘 Course 2.0
    https://pysql.tecladocode.com/
    
    › Retrieve Data → SELECT statement;
    › asterisk (`*`) is as `ALL COLUMNS`
    
    ``` sql
    -- get ALL columns from `user` table;
    -- asterisk (`*`) is as `ALL COLUMNS`
    SELECT * FROM user;
    
    SELECT first_name, surname FROM user;
    
    -- get a single column in the result
    SELECT first_name FROM user;
    ```
    
    » Learning Journal/Programming Diary with Python & SQLite :snake: :minidisc:
    › 03-11 SELECT: retrieve data | The Complete Python/PostgreSQL Course 2.0
    https://pysql.tecladocode.com/section03/lectures/11_retrieving_data/

--- learning-journal/select-all.sql
+++ learning-journal/select-all.sql
@@ -1,1 +1,8 @@
+-- get ALL columns from `user` table;
+-- asterisk (`*`) is as `ALL COLUMNS`
 SELECT * FROM user;
+
+SELECT first_name, surname FROM user;
+
+-- get a single column in the result
+SELECT first_name FROM user;
